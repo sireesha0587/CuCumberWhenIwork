070f084c0df82a3a8f9ae1bed0e8b832c0e33934
Sireesha <74946651+sireesha0587@users.noreply.github.com>
2021-10-20 17:04:27 -0500

    Add files via upload

--- my_stepslogin.rb
+++ my_stepslogin.rb
@@ -1,24 +1,24 @@
-Given(/^user launch website "https:\/\/wheniwork\.com\/"$/) do |_|
+Given(%r{^user launch website "https://wheniwork\.com/"$}) do |_|
   pending
 end
 
-Given(/^user on the Login Page "https:\/\/login\.wheniwork\.com\/"$/) do |arg|
+Given(%r{^user on the Login Page "https://login\.wheniwork\.com/"$}) do |_arg|
   pending
 end
 
-And(/^user enters "([^"]*)" with "([^"]*)"$/) do |arg1, arg2|
+And(/^user enters "([^"]*)" with "([^"]*)"$/) do |_arg1, _arg2|
   pending
 end
 
-And(/^user click "([^"]*)" button$/) do |arg|
+And(/^user click "([^"]*)" button$/) do |_arg|
   pending
 end
 
-Then(/^user should see "([^"]*)"$/) do |arg|
+Then(/^user should see "([^"]*)"$/) do |_arg|
   pending
 end
 
-Given(/^the user is on the Home page "([^"]*)"https:\/\/wheniwork\.com\/"$/) do |arg|
+Given(%r{^the user is on the Home page "([^"]*)"https://wheniwork\.com/"$}) do |_arg|
   pending
 end
 
@@ -26,7 +26,7 @@ And(/^the user navigates to the Login page$/) do
   pending
 end
 
-And(/^the user enters (.*) and (.*)$/) do |username, password, table|
+And(/^the user enters (.*) and (.*)$/) do |_username, _password, _table|
   # table is a table.hashes.keys # => [:username, :password]
   pending
 end
